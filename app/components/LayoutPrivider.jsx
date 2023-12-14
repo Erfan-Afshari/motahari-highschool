20fc77f721d2cc0e1526b7c31de0aacafccc2443
unknown <erfanafshar8787@gmail.com>
2023-12-14 14:03:22 +0330

    update

--- app/components/LayoutPrivider.jsx
+++ app/components/LayoutPrivider.jsx
@@ -1,7 +1,6 @@
 'use client'
 import Footer from '@/app/components/Footer';
 import Navbar from '@/app/components/Navbar';
-// Use usePathname for catching route name.
 import { usePathname } from 'next/navigation';
 
 export const LayoutProvider = ({ children }) => {
